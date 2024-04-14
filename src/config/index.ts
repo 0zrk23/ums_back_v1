15fe12c434f44c6c63289fbe4a53b0df051e0330
Gustavo Carrillo <g.carrillo@softtek.com>
2024-04-13 23:21:22 -0600

    Fix attempt for deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,10 +7,9 @@ const env = cleanEnv(process.env, {
   PORT: port({ default: 3000 }),
   MONGO_URL: str(),
   SECRET_KEY: str(),
-  LOG_FORMAT: str(),
   LOG_DIR: str(),
   ORIGIN: str(),
   CREDENTIALS: bool(),
 });
 
-export const { PORT, MONGO_URL, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, CREDENTIALS } = env;
+export const { PORT, MONGO_URL, SECRET_KEY, LOG_DIR, ORIGIN, CREDENTIALS } = env;
